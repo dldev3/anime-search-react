cd3f7c5786838fa90cf38567079aac1f6f6f5a79
d_liyanage <admliyanage@gmail.com>
2021-01-12 08:30:18 +0530

    finished

---
+++ src/components/AnimeCard.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const AnimeCard = ({anime}) => {
+    return (
+        <article className="anime-card">
+            <a href={anime.url}>
+                <figure>
+                    <img
+                        src={anime.image_url}
+                        alt="Anime Image"
+                    />
+                </figure> 
+                <h3>{ anime.title}</h3>
+            </a>
+        </article>
+    )
+}
+
+export default AnimeCard
